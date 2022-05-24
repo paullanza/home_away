b20b377f21777d8f4b341e7d498ce28ca5f064d8
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-05-24 11:20:02 -0400

    validations on participation table done

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,7 @@
 class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :user_id, presence: true, uniqueness: true
+  validates :event_id, presence: true, uniqueness: true
 end
