f9e48899ca79613b5bbd1f6ebebf9a906fe65a4b
Paul Lanza <paullanza@gmail.com>
2022-05-24 14:32:00 -0400

    fix: Seed, validations and relations complete.

--- app/models/users_expertise.rb
+++ app/models/users_expertise.rb
@@ -1,4 +1,5 @@
 class UsersExpertise < ApplicationRecord
   belongs_to :user
   belongs_to :expertise
+  validates :user, :expertise, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+UsersExpertise.destroy_all
+Expertise.destroy_all
+User.destroy_all
+
+
+user = User.create({
+  email: 'b@b.b',
+  password: 123456,
+  origin: 'France',
+  residence: 'Canada',
+  first_name: 'Jimmy',
+  last_name: 'Johnny',
+  biography: 'Oh man, all the things I could write here!',
+})
+
+exp = Expertise.create(name: "Magician")
+
+UsersExpertise.create(user: user, expertise: exp)
