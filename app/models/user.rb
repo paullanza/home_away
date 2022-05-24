6b3c9152fca2f9f4d2e6e49b57ed5ec64b6ad1a8
Paul Lanza <paullanza@gmail.com>
2022-05-24 11:58:06 -0400

    func: Adding Validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  belongs_to :expertise
+  has_many :messages
+  has_many :chatrooms, through: :messages
+  has_many :events
+  has_many :participations
+  # validates :origin, :residence, :first_name, :last_name, presence: true
 end
