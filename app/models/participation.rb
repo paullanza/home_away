a50e72764ea96d718b2cd3334ba040958b744f31
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-05-24 11:34:14 -0400

    scope the event for its uniqueness

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,6 +2,6 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :user_id, presence: true, uniqueness: true
-  validates :event_id, presence: true, uniqueness: true
+  validates :user_id, presence: true, uniqueness: { scope: :event_id }
+#   validates :event_id, presence: true,
 end
