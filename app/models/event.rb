1127698dab5695352a9ec48a8da2498d54d36189
Paul Lanza <paullanza@gmail.com>
2022-05-24 11:12:58 -0400

    func: Validations added, need to complete Users and Categories before testing.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  has_many :participations
+  validates :title, uniqueness: true
+  validates :title, :date, :location, :description, :category, presence: true
+  validates :descritpion, length: { minimum: 25 }
 end
