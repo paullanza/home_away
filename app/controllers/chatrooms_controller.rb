df88e8306c5a1aa54014b93c96c30d21dea25990
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-25 17:14:25 -0400

    commented the controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,16 +1,22 @@
 class ChatroomsController < ApplicationController
   def show
+    # create a new message
     @message = Message.new
+    # find a chatroom
     @chatroom = Chatroom.find(params[:id])
+    # authorize it
     authorize @chatroom, policy_class: ChatroomPolicy
   end
 
   def create
+    # create a new chatroom with the strong params and authorize it
     @chatroom = Chatroom.new(chatrooms_params)
     authorize @chatroom, policy_class: ChatroomPolicy
 
+    # assignt current_user as user 1
     @chatroom.user1 = current_user
 
+    # if @chatroom saves. redirect to the chatroom
     if @chatroom.save
       redirect_to chatroom_path(@chatroom)
     end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,7 @@
 class Chatroom < ApplicationRecord
   has_many :messages
+
+  # belong to user as user1 or user2
   belongs_to :user1, class_name: "User", foreign_key: :user1_id
   belongs_to :user2, class_name: "User", foreign_key: :user2_id
 end
