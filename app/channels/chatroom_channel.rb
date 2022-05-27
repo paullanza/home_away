3b723018079bedb1252db37e5f06f42f8cd12013
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-26 21:21:59 -0400

    added ajax but the field dont empty

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,12 @@
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe
+  end
+end
