d375b50957a1f38c2b3fd983ac1d25dd8ecb1c25
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-30 11:47:09 -0400

    now search by categories

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,25 @@
 class DashboardsController < ApplicationController
   def dashboard
     # Find all the User with the same country of origin and residence as current_user
-    @profiles = User.where("origin = ? and residence = ?", current_user.origin, current_user.residence)
+    if params[:expertise_id].present?
+      @profiles = User.joins(:expertises).where("(origin = ? and residence = ?) and expertises.id = ?", current_user.origin, current_user.residence, params[:expertise_id])
+    else
+      @profiles = User.where("origin = ? and residence = ?", current_user.origin, current_user.residence)
+    end
     # remove current_user from the array so it do not appear on the dashboard
-    @profiles -= [current_user]
+    # @profiles -= [current_user]
 
     # find all events in my country of residence
-    @events = Event.joins(:user).where("location = ? and origin = ? ", current_user.residence, current_user.origin)
+    if params[:category_id].present?
+      @events = Event.joins(:user).joins(:category).where("(location = ? and origin = ?) and categories.id = ?", current_user.residence, current_user.origin, params[:category_id])
+    else
+      @events = Event.joins(:user).where("location = ? and origin = ?", current_user.residence, current_user.origin)
+    end
+
+
+    @my_chat = Chatroom.where('user1_id = ? or user2_id = ?', current_user, current_user)
+
+    @new_chatroom = Chatroom.new
+
   end
 end
