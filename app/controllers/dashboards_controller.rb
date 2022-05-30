aa9cf0d296be3205fc7a8c9149caddcd695eb2e0
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-30 15:52:35 -0400

    ok you can merge this one

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,20 +2,25 @@ class DashboardsController < ApplicationController
   def dashboard
     # Find all the User with the same country of origin and residence as current_user
     if params[:expertise_id].present?
+      # if query search by query
       @profiles = User.joins(:expertises).where("(origin = ? and residence = ?) and expertises.id = ?", current_user.origin, current_user.residence, params[:expertise_id])
     else
+      # if not query search only by origin and residence
       @profiles = User.where("origin = ? and residence = ?", current_user.origin, current_user.residence)
     end
-    # remove current_user from the array so it do not appear on the dashboard
-    # @profiles -= [current_user]
+
+    # remove current user from the array
+    @profiles -= [current_user]
 
     # find all events in my country of residence
     if params[:category_id].present?
+      # if query search by category
       @events = Event.joins(:user).joins(:category).where("(location = ? and origin = ?) and categories.id = ?", current_user.residence, current_user.origin, params[:category_id])
     else
       @events = Event.joins(:user).where("location = ? and origin = ?", current_user.residence, current_user.origin)
     end
 
+    # for the marker to appear no the map
     @markers = @events.geocoded.map do |event|
       {
         lat: event.latitude,
@@ -25,9 +30,7 @@ class DashboardsController < ApplicationController
         # image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
       }
     end
-
-    @my_chat = Chatroom.where('user1_id = ? or user2_id = ?', current_user, current_user)
-
+    # create new chat if chatroom not found in dashboard.html
     @new_chatroom = Chatroom.new
   end
 end
