ea517336c5847c9fa8ae7377d92e7ea3f5cba962
Jose Barbosa <nato.barbosa@outlook.com>
2022-05-31 12:08:04 -0400

    finished the styling for the chatroom page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,11 +7,14 @@ class MessagesController < ApplicationController
 
     @message.chatroom = @chatroom
     @message.user = current_user
+
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
+        message: render_to_string(partial: "message", locals: { message: @message }),
+        sender_id: @message.user.id
       )
+
       head :ok
     else
       render "chatrooms/show"
