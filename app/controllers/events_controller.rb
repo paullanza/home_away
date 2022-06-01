c7bf4b4d9ad5d9d8bd32c9213f00fa37761c4b5d
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-06-01 11:25:04 -0400

    x people going

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,13 +52,14 @@ class EventsController < ApplicationController
 
     # find our participation if we have one
     @my_participation = Participation.find_by("user_id = ? and event_id = ? ", current_user, @event)
+    @participations = Participation.where(event: @event)
 
     @markers = @event.geocode.map do
       {
         lat: @event.latitude,
         lng: @event.longitude,
         # info window for each marker
-        info_window: render_to_string(partial: "info_window", locals: { event: @event })
+        # info_window: render_to_string(partial: "info_window", locals: { event: @event })
         # image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
       }
     end
