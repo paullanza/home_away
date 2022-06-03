56d0a26df4f781eb7a41151b51294b3d7a253ecb
Paul Lanza <paullanza@gmail.com>
2022-06-03 10:09:23 -0400

    finished?

--- db/seeds.rb
+++ db/seeds.rb
@@ -111,7 +111,7 @@ carlos = User.create({
                       residence: 'Montreal',
                       first_name: 'Carlos',
                       last_name: 'Ribeiro',
-                      biography: 'Growing up in Brazil, I always dreamed of seeing the world. After travelling with my family for many years, we finally decided to move to Montreal. I love playing board game'
+                      biography: 'Growing up in Brazil, I always dreamed of seeing the world. After travelling with my family for many years, we finally decided to move to Montreal. I love soccer and playing board games!'
                       })
 
 user = User.create({
@@ -277,8 +277,8 @@ UsersExpertise.create(user: user15, expertise: web)
 UsersExpertise.create(user: user16, expertise: finance)
 UsersExpertise.create(user: user16, expertise: out_sport)
 
-
 puts "creating categories"
+
 shoe_exchange = Category.create(name: "Shoe Exchange")
 night_life= Category.create(name: "Night Life")
 cultural = Category.create(name: "Cultural")
@@ -293,6 +293,7 @@ cinema = Category.create(name: "Cinema")
 music = Category.create(name: "Music")
 
 puts "creating events"
+
 dinner = Event.create({
                         title: "Dinner at Le Milsa",
                         date: Date.new(2022, 05, 30),
